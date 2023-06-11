6096bd337e373bd3f991a18804070257a0586e06
tr2cky <fatih.kilic@boun.edu.tr>
2023-06-11 12:47:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a job search application developed using React Native.
+
+<img src="assets/preview/preview1.png" alt="preview1" width="350px" height="700px">
+<img src="assets/preview/preview2.png" alt="preview2" width="350px" height="700px">
+<img src="assets/preview/preview3.png" alt="preview3" width="350px" height="700px">
---
+++ hook/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RAPID_API_KEY } from "@env";
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: { ...query },
+        headers: {
+            'X-RapidAPI-Key': RAPID_API_KEY,
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+    };
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            alert('There was an error fetching data.');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const refetch = () => {
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
